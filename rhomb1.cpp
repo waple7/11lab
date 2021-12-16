7a020b01f80a1b832f96c38b8b74407c313b1c5e
waple7 <91288621+waple7@users.noreply.github.com>
2021-12-16 21:58:50 +0300

    Update rhomb1.cpp

--- rhomb1.cpp
+++ rhomb1.cpp
@@ -1,4 +1,4 @@
-#include<math.h>
+#include <math.h>
 #include "rhomb1.h"
 
 float Rhomb_square(struct rhomb coordinate) {
@@ -11,11 +11,11 @@ float Rhomb_square(struct rhomb coordinate) {
 
 float Rhomb_perimeter(struct rhomb coordinate) {
 	float AB, BC, CD, DA;
-	BC = sqrt(pow((coordinate.y1 - coordinate.z1), 2) + pow((coordinate.y2 - coordinate.z2) , 2));
 	AB = sqrt(pow((coordinate.x1 - coordinate.y1), 2) + pow((coordinate.x2 - coordinate.y2) , 2));
-	DA = sqrt(pow((coordinate.w1 - coordinate.x1), 2) + pow((coordinate.w2 - coordinate.x2) , 2));
+	BC = sqrt(pow((coordinate.y1 - coordinate.z1), 2) + pow((coordinate.y2 - coordinate.z2) , 2));
 	CD = sqrt(pow((coordinate.z1 - coordinate.w1), 2) + pow((coordinate.z2 - coordinate.w2) , 2));
+	DA = sqrt(pow((coordinate.w1 - coordinate.x1), 2) + pow((coordinate.w2 - coordinate.x2) , 2));
 	
-	return BC + AB + DA + CD;
+	return AB + BC + CD + DA;
 }
 
