ca7ed4c90fd274d65714ad93abca91b25a5783db
waple7 <91288621+waple7@users.noreply.github.com>
2021-12-16 21:52:19 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,14 +3,18 @@
 #include"rhomb1.h"
 
 int main() {
+	
 	struct rhomb arg = { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
 	scanf(" %f",&arg.x1);scanf(" %f",&arg.x2);
 	scanf(" %f",&arg.y1);scanf(" %f",&arg.y2);
 	scanf(" %f",&arg.z1);scanf(" %f",&arg.z2);
 	scanf(" %f",&arg.w1);scanf(" %f",&arg.w2);
+	
+	
 	float perimeter = Rhomb_perimeter(arg);
 	float square = Rhomb_square(arg);
-	printf("square = %f\n", square);
 	printf("perimeter = %f", perimeter);
+	printf("square = %f\n", square);
+	
 	return 0;
 }
